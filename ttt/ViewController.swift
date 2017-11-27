6df58659f1ac4a09a422e149146fa7c249e756bd
SBC <rithnguon>
2017-11-27 19:15:58 +0700

    Initial Commit

---
+++ ttt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by RithyMac on 11/27/17.
+//  Copyright © 2017 RTC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
